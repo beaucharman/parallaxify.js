4caca7db17e90c3d1c3f9ba1d9f5bd596a6537e6
Beau Charman <beau.charman@gmail.com>
2013-05-09 00:44:32 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Parallaxify
+
+Easily add a subtle parallax effect to targeted areas (from here refered to as panels) of a web page.
+
+Firstly, add the following styles to the panels (regardless of having the parallax effect or not)
+
+```css
+.panel {
+  background-position : 0 0;
+  background-repeat : no-repeat;
+  -webkit-background-size : cover;
+  -moz-background-size : cover;
+  -o-background-size : cover;
+  background-size : cover;
+}
+```
+
+Then, each parallax panel will have it's own background-image and a background-attachment of fixed.
+
+```css
+  background-image: url(../images/hero-image.jpg);
+  background-attachment : fixed;
+```
+
+Any panel that is not to be parallaxified can have a background-attachment of either fixed or scroll.
---
+++ parallaxify.js
@@ -0,0 +1,49 @@
+/**
+ * Parallaxify
+ * ------------------------------------------------------------------------
+ * parallaxify()
+ * @version 1.0 | May 8th 2013
+ * @author  Beau Charman | @beaucharman | http://beaucharman.me
+ * @param {integer} defaultSpeed | any number from 1 (hare) to 50 (turtle)
+ * ------------------------------------------------------------------------ */
+$.fn.extend({
+  parallaxify : function(defaultSpeed){
+
+    /* Setup */
+    var yPos;
+    var coords;
+    var $window   = $(window);
+    var speedAttr = 'speed';
+    defaultSpeed  = defaultSpeed || 10;
+
+    return this.each(function(){
+      /* assign the object as a new instance */
+      var $bg = $(this);
+      var speed = $bg.data(speedAttr) || defaultSpeed;
+      $window.scroll(function() {
+        yPos = -(($window.scrollTop() - $bg.offset().top) / speed);
+        coords = '0 ' + yPos + 'px';
+        $bg.css({
+          backgroundPosition: coords
+        });
+      });
+    });
+  }
+});
+
+/**
+ * Fit Vertically
+ * ------------------------------------------------------------------------
+ * fitVertically()
+ * @version 1.0 | May 8th 2013
+ * @author  Beau Charman | @beaucharman | http://beaucharman.me
+ *
+ * Change the target elements height to be that of the window's
+ * ------------------------------------------------------------------------ */
+$.fn.extend({
+  fitVertically : function(){
+    return this.each(function(){
+      $(this).height($(window).height());
+    });
+  }
+});

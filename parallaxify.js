e1bd2a7bd5b9a147983060bfdb99860e06e4985f
Beau Charman <beau.charman@gmail.com>
2013-05-15 00:16:01 +1000

    Core changes to plugin setup

--- parallaxify.js
+++ parallaxify.js
@@ -4,23 +4,19 @@
  * parallaxify()
  * @version 1.0 | May 8th 2013
  * @author  Beau Charman | @beaucharman | http://beaucharman.me
- * @param {integer} defaultSpeed | any number from 1 (hare) to 50 (turtle)
+ * @param   {integer} defaultSpeed | any number from 1 (hare) to 50 (turtle)
  * ------------------------------------------------------------------------ */
-$.fn.extend({
-  parallaxify : function (defaultSpeed) {
-
-    /* Setup */
-    var yPos;
-    var coords;
-    var $window   = $(window);
-    var speedAttr = 'speed';
-    defaultSpeed  = defaultSpeed || 20;
-
+;(function ($) {
+  $.fn.parallaxify = function (options) {
+    var settings = $.extend({
+      'defaultSpeed': 20,
+      'speedAttr': '[data-speed]'
+    }, options);
+    var yPos, coords;
+    var $window = $(window);
     return this.each(function () {
-      /* assign the object as a new instance */
       var $bg = $(this);
-      var speed = $bg.data(speedAttr) || defaultSpeed;
-
+      var speed = $bg.attr(settings.speedAttr) || settings.defaultSpeed;
       $window.scroll(function () {
         yPos = -(($window.scrollTop() - $bg.offset().top) / speed);
         coords = '0 ' + yPos + 'px';
@@ -29,8 +25,8 @@ $.fn.extend({
         });
       });
     });
-  }
-});
+  };
+})(jQuery);
 
 /**
  * Fit Vertically
@@ -41,10 +37,10 @@ $.fn.extend({
  *
  * Change the target elements height to be that of the window's
  * ------------------------------------------------------------------------ */
-$.fn.extend({
-  fitVertically : function () {
+;(function ($) {
+  $.fn.fitVertically = function () {
     return this.each(function () {
       $(this).height($(window).height());
     });
-  }
-});
+  };
+})(jQuery);
